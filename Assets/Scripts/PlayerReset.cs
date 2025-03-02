09c030062ec58d145369670430e265832cfe13f3
JlordS32 <jlordsty128@gmail.com>
2025-03-02 13:13:01 +1100

    Core resetting feature done

--- Assets/Scripts/PlayerReset.cs
+++ Assets/Scripts/PlayerReset.cs
@@ -1,19 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
-using StarterAssets;
-using UnityEngine.InputSystem;
 
 public class PlayerReset : MonoBehaviour
 {
+    [Header("Prefabs")]
+    [SerializeField] private GameObject checkpointPrefab;
+    [SerializeField] private Transform parentFolder;
+
     [Header("Testing")]
     private readonly KeyCode resetKey = KeyCode.R;
     private readonly KeyCode checkpointKey = KeyCode.T;
 
-    private Vector3 checkpointPosition;
-
-    void Awake()
-    {
-        checkpointPosition = transform.position;
-    }
+    private GameObject currentCheckpoint;
 
     void Update()
     {
@@ -22,17 +20,39 @@ public class PlayerReset : MonoBehaviour
             UpdateCheckPoint();
         }
 
-        if (Input.GetKeyDown(resetKey)) {
+        if (Input.GetKeyDown(resetKey))
+        {
             ResetPosition();
         }
     }
 
-    void UpdateCheckPoint() {
-        checkpointPosition = transform.position;
+    void UpdateCheckPoint()
+    {
+        // Check if the parent folder is set, please set it first.
+        if (parentFolder == null)
+        {
+            Debug.LogWarning("Parent folder is null, please set one to organise checkpoints");
+            return;
+        }
+
+        // If a checkpoint already exists, destroy the previous one
+        if (currentCheckpoint != null)
+        {
+            Destroy(currentCheckpoint);
+        }
+
+        // Instantiate new checkpoint and store the reference to currentCheckpoint (We'll destroy it later)
+        currentCheckpoint = Instantiate(checkpointPrefab, transform.position, Quaternion.identity, parentFolder);
     }
 
     void ResetPosition()
     {
-        transform.position = checkpointPosition;  
+        if (currentCheckpoint == null) return;
+
+        if (currentCheckpoint != null)
+        {
+            Destroy(currentCheckpoint);
+            transform.position = currentCheckpoint.transform.position;
+        }
     }
 }
