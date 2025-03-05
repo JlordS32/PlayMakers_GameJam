3b0e855dd3c49433c0e52150bee032e2cfd1860b
DuyNguyen16 <khanhduyn161@gmail.com>
2025-03-05 16:19:24 +1100

    Power Up Spawner

---
+++ Assets/Scripts/Objects/PowerSpawner.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private float y;
+    [SerializeField] private float spawnTime;   
+
+
+    public GameObject prefab;
+    public Transform spawnerBase;
+    public GameObject currentObject;
+
+    void checkAndSpawn()
+    {
+        if (currentObject == null)
+        {
+            spawnObject();
+        }
+    }
+
+    void spawnObject()
+    {
+        GameObject player = GameObject.Find("Player");
+
+        PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+        // Check if the player has extra jumps
+        if (playerMovement.getExtraJumps() == 0)
+        {
+            // Get the platform's position
+            Vector3 spawnPosition = spawnerBase.position + new Vector3(0, y, 0);
+
+            // Spawn the object on the platform
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+
+    void Start()
+    {
+
+        spawnObject();
+        InvokeRepeating("checkAndSpawn", spawnTime, spawnTime);
+
+    }
+}
